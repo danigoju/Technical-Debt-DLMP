9958006e6b0b662725425179482e2c848b6a0105
marinarosell <48138799+marinarosell@users.noreply.github.com>
2021-10-18 10:15:53 +0200

    added a preprocess file

---
+++ src/features/preprocess.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+def preprocess(git_commits, szz_fault_inducing_commits, refactoring_miner):
+	'''
+	
+	'''
+	# keep columns and rows of interest of each dataframe individually
+	git_commits = git_commits[['PROJECT_ID','COMMIT_HASH','COMMIT_MESSAGE','COMMITTER_DATE','BRANCHES']]
+	szz_fault_inducing_commits = szz_fault_inducing_commits[["FAULT_FIXING_COMMIT_HASH", "FAULT_INDUCING_COMMIT_HASH"]]
+	refactoring_miner = refactoring_miner[["COMMIT_HASH", "REFACTORING_TYPE"]].drop_duplicates(ignore_index=True)
+
+	# merge of dataframes
+	
+	refactoring_fault_inducing = refactoring_miner.merge(szz_fault_inducing_commits, left_on="COMMIT_HASH", right_on="FAULT_INDUCING_COMMIT_HASH")
+	refactoring_fault_inducing = refactoring_fault_inducing.drop("COMMIT_HASH", axis=1)
+
+	fault_fixing_commits = refactoring_fault_inducing.merge(git_commits, left_on="FAULT_FIXING_COMMIT_HASH", right_on="COMMIT_HASH")
+	fault_fixing_commits = fault_fixing_commits.drop("COMMIT_HASH", axis=1)
+
+	return fault_fixing_commits
