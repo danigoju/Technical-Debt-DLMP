62777e1d120a99c21f9ba5c7cb36367b968c6821
Driux <48140294+driux@users.noreply.github.com>
2021-10-19 14:33:45 +0200

    Notebook de modelos

--- src/features/preprocess.py
+++ src/features/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def preprocess(git_commits, szz_fault_inducing_commits, refactoring_miner):
+def preprocess(git_commits, szz_fault_inducing_commits, refactoring_miner, git_commits_changes):
 	'''
 	
 	'''
@@ -9,6 +9,7 @@ def preprocess(git_commits, szz_fault_inducing_commits, refactoring_miner):
 	git_commits = git_commits[['PROJECT_ID','COMMIT_HASH','COMMIT_MESSAGE','COMMITTER_DATE','BRANCHES']]
 	szz_fault_inducing_commits = szz_fault_inducing_commits[["FAULT_FIXING_COMMIT_HASH", "FAULT_INDUCING_COMMIT_HASH"]]
 	refactoring_miner = refactoring_miner[["COMMIT_HASH", "REFACTORING_TYPE"]].drop_duplicates(ignore_index=True)
+	git_commits_changes = git_commits_changes[["COMMIT_HASH", "COMMITTER_ID","LINES_ADDED","LINES_REMOVED"]].drop_duplicates(ignore_index=True)
 
 	# merge of dataframes
 	
@@ -16,6 +17,9 @@ def preprocess(git_commits, szz_fault_inducing_commits, refactoring_miner):
 	refactoring_fault_inducing = refactoring_fault_inducing.drop("COMMIT_HASH", axis=1)
 
 	fault_fixing_commits = refactoring_fault_inducing.merge(git_commits, left_on="FAULT_FIXING_COMMIT_HASH", right_on="COMMIT_HASH")
+    
+	fault_fixing_commits = fault_fixing_commits.merge(git_commits_changes, how='left', on='COMMIT_HASH')
 	fault_fixing_commits = fault_fixing_commits.drop("COMMIT_HASH", axis=1)
+
 
 	return fault_fixing_commits
